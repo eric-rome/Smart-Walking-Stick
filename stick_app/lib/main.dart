d9de05300a072f2a9dc64431023c0b1b47a98fff
eric-rome <ericrmerichal@gmail.com>
2025-09-19 10:08:57 +0200

    Add files via upload

---
+++ stick_app/lib/main.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:stick_app/screens/start_screen.dart';
+import 'package:stick_app/screens/login_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Safety App',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const SplashScreen(), // Cambiamos StartScreen por SplashScreen
+    );
+  }
+}
+
+class SplashScreen extends StatelessWidget {
+  const SplashScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+      future: _getInitialScreen(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Scaffold(
+            body: Center(child: CircularProgressIndicator()),
+          );
+        } else if (snapshot.hasData) {
+          return snapshot.data as Widget;
+        } else {
+          return const LoginScreen();
+        }
+      },
+    );
+  }
+
+  Future<Widget> _getInitialScreen() async {
+  return const StartScreen();
+
+}
+}
